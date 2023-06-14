299353ac5db2fbc28cc571976fa75939ca11d3da
cwahlund <wahlund@icloud.com>
2023-06-13 17:13:46 -0700

    added code to DbException

--- src/main/java/projects/exception/DbException.java
+++ src/main/java/projects/exception/DbException.java
@@ -3,10 +3,16 @@ package projects.exception;
 @SuppressWarnings("serial")
 public class DbException extends RuntimeException {
 
-	public DbException(String message) {}
+	public DbException(String message) {
+		super(message);
+	}
 	
-	public DbException(Throwable cause) {}
+	public DbException(Throwable cause) {
+		super(cause);
+	}
 	
-	public DbException(String message, Throwable cause) {}
+	public DbException(String message, Throwable cause) {
+		super(message, cause);
+	}
 	
 }
